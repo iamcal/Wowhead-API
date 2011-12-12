9e3bea8b2bb0d6c1113dd1870c42fd9e67d584a5
Cal Henderson <cal@iamcal.com>
2011-12-12 02:04:01 -0600

    switching from simpletest to testmore

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?
-	require_once(dirname(__FILE__) . '/simpletest/autorun.php');
+	include('testmore.php');
 
 	$cfg = array(
 		'http_timeout' => 5,
@@ -9,39 +9,35 @@
 	include('lib_json.php');
 	include('lib_wowhead.php');
 
+	plan(13);
+
 
 	#
-	# let the tests begin!
+	# simple achievements fetch
 	#
 
-	class WowheadAPITests extends UnitTestCase {
+	$ret = wowhead_achievement(2537);
+	ok($ret['ok'], "Achievement fetch returned ok");
+	is($ret['title'], "Mountain o' Mounts", "Achievement title");
+	is($ret['description'], "Obtain 100 mounts.", "Achievement description");
+	is($ret['icon'], 'ability_hunter_pet_dragonhawk', "Achievement icon");
+	is($ret['points'], 10, "Achievement points");
+	is($ret['faction'], 'horde', "Achievement faction");
 
-		function testAchievements(){
 
-			$ret = wowhead_achievement(2537);
-			$this->assertTrue($ret['ok']);
+	#
+	# quest categories
+	#
 
-			$this->assertEqual($ret['title'], "Mountain o' Mounts");
-			$this->assertEqual($ret['description'], "Obtain 100 mounts.");
-			$this->assertEqual($ret['icon'], 'ability_hunter_pet_dragonhawk');
-			$this->assertEqual($ret['points'], 10);
-			$this->assertEqual($ret['faction'], 'horde');
-		}
+	$ret = wowhead_quest_cats();
+	ok($ret['ok'], "Quest cats returned ok");
 
-		function testQuests(){
+	cmp_ok(count($ret['cats']), '>', 1, "Quests have multiple categories");
 
-			$ret = wowhead_quest_cats();
-			$this->assertTrue($ret['ok']);
+	is($ret['cats'][7]['title'], 'Miscellaneous', "Quest cat 7 title");
+	is($ret['cats'][7]['id'], 7, "Quest cat 7 ID");
 
-			$this->assertTrue(count($ret['cats']) > 0);
+	cmp_ok(count($ret['cats'][7]['subcats']), '>', 1, "Quest cat 7 has multiple sub cats");
 
-			$this->assertEqual($ret['cats'][7]['title'], 'Miscellaneous');
-			$this->assertEqual($ret['cats'][7]['id'], 7);
-
-			$this->assertTrue(count($ret['cats'][7]['subcats']) > 0);
-
-			$this->assertEqual($ret['cats'][7]['subcats'][-379]['title'], 'Firelands Invasion');
-			$this->assertEqual($ret['cats'][7]['subcats'][-379]['id'], -379);
-		}
-	}
-
+	is($ret['cats'][7]['subcats'][-379]['title'], 'Firelands Invasion', "Quest cat 7.-379 title");
+	is($ret['cats'][7]['subcats'][-379]['id'], -379, "Quest cat 7.-379 ID");
